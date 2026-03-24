3c48cde0a4100e619b424feab513165e7fc706a0
willyjohne2 <johnfaii285@gmail.com>
2026-03-24 14:44:42 +0300

    Fix case-sensitivity in MpesaHandler to check for lowercase settings first

--- apps/utils/mpesa.py
+++ apps/utils/mpesa.py
@@ -9,21 +9,22 @@ from .encryption import get_setting
 
 class MpesaHandler:
     def __init__(self):
-        self.consumer_key = get_setting("MPESA_CONSUMER_KEY", getattr(settings, "MPESA_CONSUMER_KEY", ""))
-        self.consumer_secret = get_setting("MPESA_CONSUMER_SECRET", getattr(settings, "MPESA_CONSUMER_SECRET", ""))
-        self.shortcode = get_setting("MPESA_SHORTCODE", getattr(settings, "MPESA_SHORTCODE", ""))
-        self.passkey = get_setting("MPESA_PASSKEY", getattr(settings, "MPESA_PASSKEY", ""))
+        # Keys are stored in lowercase in DB (from seed/frontend), so we check lowercase first
+        self.consumer_key = get_setting("mpesa_consumer_key", get_setting("MPESA_CONSUMER_KEY", getattr(settings, "MPESA_CONSUMER_KEY", "")))
+        self.consumer_secret = get_setting("mpesa_consumer_secret", get_setting("MPESA_CONSUMER_SECRET", getattr(settings, "MPESA_CONSUMER_SECRET", "")))
+        self.shortcode = get_setting("mpesa_shortcode", get_setting("MPESA_SHORTCODE", getattr(settings, "MPESA_SHORTCODE", "")))
+        self.passkey = get_setting("mpesa_passkey", get_setting("MPESA_PASSKEY", getattr(settings, "MPESA_PASSKEY", "")))
         self.b2c_shortcode = get_setting(
             "mpesa_b2c_shortcode", 
             os.getenv('MPESA_B2C_SHORTCODE', '600996')
         )
-        self.initiator_name = get_setting("MPESA_INITIATOR_NAME", getattr(settings, "MPESA_INITIATOR_NAME", "testapi"))
+        self.initiator_name = get_setting("mpesa_initiator_name", get_setting("MPESA_INITIATOR_NAME", getattr(settings, "MPESA_INITIATOR_NAME", "testapi")))
         self.initiator_password = get_setting(
-            "MPESA_INITIATOR_PASSWORD", getattr(settings, "MPESA_INITIATOR_PASSWORD", "Safaricom007*")
+             "mpesa_initiator_password", get_setting("MPESA_INITIATOR_PASSWORD", getattr(settings, "MPESA_INITIATOR_PASSWORD", "Safaricom007*"))
         )
-        self.callback_url = get_setting("MPESA_CALLBACK_URL", getattr(settings, "MPESA_CALLBACK_URL", ""))
+        self.callback_url = get_setting("mpesa_callback_url", get_setting("MPESA_CALLBACK_URL", getattr(settings, "MPESA_CALLBACK_URL", "")))
 
-        env = get_setting("MPESA_ENVIRONMENT", getattr(settings, "MPESA_ENVIRONMENT", "sandbox"))
+        env = get_setting("mpesa_environment", get_setting("MPESA_ENVIRONMENT", getattr(settings, "MPESA_ENVIRONMENT", "sandbox")))
         self.base_url = (
             "https://sandbox.safaricom.co.ke"
             if env == "sandbox"
